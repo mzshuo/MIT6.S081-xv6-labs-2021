8644c022181fc94e65707967afa4b9b13ba5ef48
leeshuo <leeshuo@pku.edu.cn>
2023-08-24 15:32:58 +0800

    finish print pagetable

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprintHelper(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; ++i) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      // print info of pte
+      for (int rep = 0; rep < level; ++rep) {
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+    }
+    if ((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      // this PTE points to a lower-level page table.
+      vmprintHelper((pagetable_t)PTE2PA(pte), level+1);
+    }
+  }
+}
+
+// Print the contents of page table pointed by pagetable.
+void vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", pagetable);
+  vmprintHelper(pagetable, 1);
+}

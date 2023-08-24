82fdb4f7b32bd7f5a4f9ebd9d1efe2333ab9b55f
leeshuo <leeshuo@pku.edu.cn>
2023-08-24 15:40:54 +0800

    finish pgaccess

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -77,10 +77,46 @@ sys_sleep(void)
 
 
 #ifdef LAB_PGTBL
+/* A system call that reports which pages have been accessed.
+ * The system call takes three arguments. 
+ * First, it takes the starting virtual address of the first user page to check. 
+ * Second, it takes the number of pages to check. 
+ * Finally, it takes a user address to a buffer to store the results into a bitmask.
+*/
 int
 sys_pgaccess(void)
 {
   // lab pgtbl: your code here.
+  uint64 base;
+  int len;
+  uint64 maskaddr;
+
+  if (argaddr(0, &base) < 0)
+    return -1;
+  if (argint(1, &len) < 0)
+    return -1;
+  if (argaddr(2, &maskaddr) < 0)
+    return -1;
+
+  if (len > 32) {
+    printf("Warning: can check only the first 32 pages of %d pages.\n", len);
+    len = 32;
+  }
+
+  unsigned int mask = 0;
+  for (int i = 0; i < len; ++i) {
+    uint64 va = base + PGSIZE * i;
+    pte_t* pte = walk(myproc()->pagetable, va, 0);
+    if (*pte & PTE_A) {
+      mask |= (1L << i);
+      *pte &= ~PTE_A;
+    }
+  }
+  
+  if (copyout(myproc()->pagetable, maskaddr, (char*)&mask, sizeof(mask)) < 0) {
+    return -1;
+  }
+  
   return 0;
 }
 #endif
